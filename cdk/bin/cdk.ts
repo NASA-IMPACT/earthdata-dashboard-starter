99b84a2a9aee5e11f2d5045b2b832e2ad549ec40
“slesaad” <slesaad@gmail.com>
2021-05-06 14:22:30 -0500

    Remove hardcoded deployment region

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -15,7 +15,7 @@ new CdkStack(app, 'CdkStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  env: { region: 'us-west-2' },
+  // env: { region: 'us-west-2' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
