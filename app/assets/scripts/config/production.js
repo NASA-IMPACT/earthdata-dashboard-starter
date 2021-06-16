d13184d31974d268b281e24ded95b84dd04e3903
Aimee Barciauskas <aimee@developmentseed.org>
2021-06-16 13:18:22 -0700

    Fix syntax

--- app/assets/scripts/config/production.js
+++ app/assets/scripts/config/production.js
@@ -13,7 +13,7 @@ module.exports = {
     appDescription: 'Explore the data using our new experimental dashboard.',
     twitterHandle: '@NASAEarthData',
     mbToken: 'pk.eyJ1IjoiY292aWQtbmFzYSIsImEiOiJja2F6eHBobTUwMzVzMzFueGJuczF6ZzdhIn0.8va1fkyaWgM57_gZ2rBMMg',
-    api: process.env.STAGE == "staging" ? process.env.STAGING_API_URL || (process.env.API_URL || 'http://localhost:8000/v1'),
+    api: process.env.STAGE == "staging" ? process.env.STAGING_API_URL : (process.env.API_URL || 'http://localhost:8000/v1'),
     map: {
       center: [0, 0],
       zoom: 2,
