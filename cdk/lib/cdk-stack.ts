cbd8c5b46d4bfd1f9b371cd572aaec89c80da08e
Aimee Barciauskas <aimee@developmentseed.org>
2021-06-19 20:20:10 -0400

    Fix cdk ts

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -3,7 +3,7 @@ import * as s3 from '@aws-cdk/aws-s3'
 import * as s3Deployment from '@aws-cdk/aws-s3-deployment'
 
 export class CdkStack extends cdk.Stack {
-  constructor(scope: ct dk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
